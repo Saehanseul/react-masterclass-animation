51edd8ba94e58a6d93f3503f8cb056d74c86465a
Saehanseul <saehanseul@gmail.com>
2024-03-12 13:54:09 +0900

    Feat: drag & scroll animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
 import styled from "styled-components";
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 35px;
+  grid-template-columns: repeat(2, 1fr);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const boxVars = {
+  start: {},
+  end: {}
+};
+
 function App() {
+  const x = useMotionValue(0);
+  const { scrollY, scrollYProgress } = useScroll();
+  const rotateZ = useTransform(x, [-400, 400], [360, -360]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 2]);
+
+  const gradient = useTransform(
+    x,
+    [-400, 400],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))"
+    ]
+  );
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    console.log("scrollY : ", latest);
+  });
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("scrollYProgress : ", latest);
+  });
+
+  useMotionValueEvent(x, "change", (latest) => {
+    console.log(latest);
+  });
   return (
-    <Wrapper>
-      <Box />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag={"x"}></Box>
     </Wrapper>
   );
 }
